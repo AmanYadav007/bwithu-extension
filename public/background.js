22bb1b1c43cd202f1bb61bfe0ffebb9e11f00df6
AmanYadav007 <aman@spotter.ai>
2026-05-09 04:13:17 +0530

    Fix connection error in background.js - handle content script not loaded

--- public/background.js
+++ public/background.js
@@ -1,4 +1,10 @@
 // Toggle BwithU visibility when extension icon is clicked
 chrome.action.onClicked.addListener(async (tab) => {
-  chrome.tabs.sendMessage(tab.id, { toggle: true });
+  try {
+    await chrome.tabs.sendMessage(tab.id, { toggle: true });
+  } catch (error) {
+    console.log('Content script not loaded yet, reloading page...');
+    // Content script might not be loaded, reload the tab
+    chrome.tabs.reload(tab.id);
+  }
 });
